7529d9764d70bebd914f8f4cac08ec1cefc6a72a
kevinnicholasalmario <kevinnicholas.almario@asurion.com>
2023-07-25 22:34:54 +0800

    Created function for creating actual json files for inline policies

--- services/policyBuilder.ts
+++ services/policyBuilder.ts
@@ -32,6 +32,27 @@ const generateExplicitActions = (serviceName: string): string[] => {
   ]
 }
 
+const createInlinePolicies = (serviceDictionary: ServiceDictionary) => {
+  for (let key in serviceDictionary) {
+    let value = serviceDictionary[key];
+
+    let basePolicy = generateEmptyBasePolicy();
+    let statement = generateEmptyStatement();
+
+    statement.Action = statement.Action.concat(value);
+
+    basePolicy.Statement.push(statement);
+
+    if (!fs.existsSync("results/test-biffy-CodeBuild-Role")) {
+      fs.mkdirSync("results/test-biffy-CodeBuild-Role");
+    }
+
+    fs.writeFileSync(`results/test-biffy-CodeBuild-Role/${key}-inline-policy.json`, JSON.stringify(basePolicy), {
+      flag: 'w'
+    });
+  }
+}
+
 const populateServiceDictionary = (serviceDictionary: ServiceDictionary, serviceNamespace: string, serviceOrAction: string) => {
   if (serviceDictionary.hasOwnProperty(serviceNamespace)) {
     serviceDictionary[serviceNamespace].push(serviceOrAction);
@@ -62,7 +83,8 @@ const processIamCsv = async (error: any, csvRecords: any) => {
       });
     }
   }
-  
+
+  createInlinePolicies(serviceDictionary);
   console.log(JSON.stringify(serviceDictionary));
 }
 
