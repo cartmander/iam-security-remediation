47dbccdb32d8e0a5a4e5817f6f8099e6298f6b85
kevinnicholasalmario <kevinnicholas.almario@asurion.com>
2023-11-23 20:41:01 +0800

    DS-9999

--- services/accessAdvisor.ts
+++ services/accessAdvisor.ts
@@ -4,11 +4,7 @@ import { client } from "../client.js";
 import path from "path";
 import fs from "fs";
 
-//DS-1234
-//DS-5678
-//Same commit number
-//DS-12345 - more ticket
-//DS-8888
+//DS-9999
 
 const generateServiceLastAccessedDetails = async ({ arn, granularity }: GenerateServiceLastAccessedDetailsCommandInput): Promise<GenerateServiceLastAccessedDetailsCommandOutput> => { 
   const serviceDetailsCommandInput = {
