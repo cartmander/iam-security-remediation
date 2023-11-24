d6142deecc5a033a03f4a192ef0adfd1c3f11657
kevinnicholasalmario <kevinnicholas.almario@asurion.com>
2023-11-24 17:25:12 +0800

    DS-5555

--- services/accessAdvisor.ts
+++ services/accessAdvisor.ts
@@ -6,6 +6,7 @@ import fs from "fs";
 
 // No ticket commit
 // DS-1234
+// DS-5555
 
 const generateServiceLastAccessedDetails = async ({ arn, granularity }: GenerateServiceLastAccessedDetailsCommandInput): Promise<GenerateServiceLastAccessedDetailsCommandOutput> => { 
   const serviceDetailsCommandInput = {
